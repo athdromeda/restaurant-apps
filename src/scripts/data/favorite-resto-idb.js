84f2420d979c778278aa1209fe1e8eeb8532624c
athdromeda <anixatho01@gmail.com>
2023-03-02 08:51:24 +0700

    fix: Handle resto with no `id`

--- src/scripts/data/favorite-resto-idb.js
+++ src/scripts/data/favorite-resto-idb.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable consistent-return */
 import { openDB } from 'idb';
 import CONFIG from '../globals/config';
 
@@ -11,14 +13,24 @@ const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
 
 const FavoriteRestoIdb = {
   async getResto(id) {
+    if (!id) {
+      return;
+    }
+
     return (await dbPromise).get(OBJECT_STORE_NAME, id);
   },
+
   async getAllRestos() {
     return (await dbPromise).getAll(OBJECT_STORE_NAME);
   },
+
   async putResto(resto) {
+    if (!resto.hasOwnProperty('id')) {
+      return;
+    }
     return (await dbPromise).put(OBJECT_STORE_NAME, resto);
   },
+
   async deleteResto(id) {
     return (await dbPromise).delete(OBJECT_STORE_NAME, id);
   },
