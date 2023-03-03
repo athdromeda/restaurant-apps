bf0680692392a50b85087156150e1a99880c89cf
athdromeda <anixatho01@gmail.com>
2023-03-03 10:03:57 +0700

    feat: Implement workbox for service worker

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,11 +1,16 @@
+import * as WorkboxWindow from 'workbox-window';
+
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
     console.log('Service worker not supported');
     return;
   }
 
+  const wb = new WorkboxWindow.Workbox('./sw.bundle.js');
+
   try {
-    await navigator.serviceWorker.register('./sw.bundle.js');
+    await wb.register();
+    // await navigator.serviceWorker.register('./sw.bundle.js');
     console.log('Service worker registeredd succesfully!');
   } catch (err) {
     console.log('Failed to register service worker', err);
